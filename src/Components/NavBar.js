1fc1c3f32d9c732567fbbc9554bcc1c04453d6ad
damarislh10 <cortezhernandezdamaris@gmail.com>
2022-06-04 14:23:32 -0500

    Styles NavBar and icons

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,28 +1,44 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "../Styles/NavBar.css";
 
 export const NavBar = () => {
   return (
-    <Container fluid>
+    <div>
       <Navbar
-        className="mb-5"
+        bg="dark"
+        variant="dark"
+        className="colorBacknav mb-5"
         collapseOnSelect
-        expand="lg"
-        bg="light"
-        variant="light"
       >
         <Container>
-          <Navbar.Brand href="#home">Bold</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">
+            <img
+              className="logo_bold"
+              src="https://res.cloudinary.com/df90q7vvj/image/upload/v1654370459/boldPruebatecnica/logobold_nyl6zp.png"
+              alt="logo_Bold"
+            />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto"></Nav>
             <Nav>
-              <Nav.Link href="#deets">Mi negocio</Nav.Link>
-              <Nav.Link href="#deets">Ayuda</Nav.Link>
+              <Nav.Link className="link-nav" href="#deets">
+                Mi negocio
+              </Nav.Link>
+              <Nav.Link className="link-nav" href="#deets">
+                Ayuda
+                <img
+                  className="ms-2 iconHelp"
+                  src="https://res.cloudinary.com/df90q7vvj/image/upload/v1654368923/boldPruebatecnica/icons8-ayuda-24_pgmhsd.png"
+                  alt="iconHelp"
+                />
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-    </Container>
+    </div>
   );
 };
